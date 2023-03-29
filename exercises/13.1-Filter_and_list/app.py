f9d7057d160ca9281a1473cb792be16a55429846
Jonathan A. Montero C <dyatroniccr@gmail.com>
2023-03-29 17:15:50 +0000

    Update ejercicio 13.2

--- exercises/13.1-Filter_and_list/app.py
+++ exercises/13.1-Filter_and_list/app.py
@@ -3,6 +3,11 @@ all_names = ["Romario","Boby","Roosevelt","Emiliy", "Michael", "Greta", "Patrici
 
 #Your code go here:
 
+def names_w_r(name):
+    return name[0] == 'R'
+        
+
+resulting_names = list(filter(names_w_r, all_names))
 print(resulting_names)
 
 
