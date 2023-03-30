1499066bf9b1b2ad33199b5639feff9897d75997
Jonathan A. Montero C <dyatroniccr@gmail.com>
2023-03-30 04:36:22 +0000

    Update 16

--- exercises/15.1-Matrix_Builder/app.py
+++ exercises/15.1-Matrix_Builder/app.py
@@ -1,3 +1,15 @@
 #Import random
 
 #Create the function below:
+def matrixBuilder(number):
+    colums = []
+    for x in range(number):
+        row = []         
+        for y in range(number):
+            row.append(1)
+
+        colums.append(row)        
+    
+    return colums
+
+print(matrixBuilder(5))
--- exercises/15.2-Parking_lot_check/app.py
+++ exercises/15.2-Parking_lot_check/app.py
@@ -5,3 +5,24 @@ parking_state = [
 ]
 
 #Your code go here:
+
+def get_parking_lot(park_matrix):
+  
+  state = {
+      "total_slots": 0,
+      "available_slots": 0,
+      "occupied_slots": 0
+  }
+
+  for row in park_matrix:       
+    for slots in row:
+      if slots > 0:
+        state["total_slots"] += 1
+      if slots == 2:
+        state["available_slots"] += 1
+      if slots == 1:
+        state["occupied_slots"] += 1
+      
+  return state
+
+print(get_parking_lot(parking_state))
