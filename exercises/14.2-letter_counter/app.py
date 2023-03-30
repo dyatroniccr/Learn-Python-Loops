5a529696ab9f22abd9a06888e2b365ce16e4628f
Jonathan A. Montero C <dyatroniccr@gmail.com>
2023-03-30 05:39:53 +0000

    Update 16

--- exercises/14.2-letter_counter/app.py
+++ exercises/14.2-letter_counter/app.py
@@ -1,11 +1,15 @@
 par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
 
-counts = {}
+counts = {"i":1}
 #your code go here:
-new_par = par.lower()
+new_par = par.lower().replace(" ", "")
 
 for letter in new_par:
     print(letter)
+    if letter in counts:
+        counts[letter] += 1
+    else:
+        counts[letter] = 1
+
 
 print(counts)
-
