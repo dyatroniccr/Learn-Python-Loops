9bfd55f8b4ff5567ddc46abcd37182205b74bb21
Jonathan A. Montero C <dyatroniccr@gmail.com>
2023-04-17 05:41:41 +0000

    Finish Exercise

--- exercises/14.2-letter_counter/app.py
+++ exercises/14.2-letter_counter/app.py
@@ -1,13 +1,13 @@
 par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
 
-counts = {"i":1}
+counts = {}
 #your code go here:
 new_par = par.lower().replace(" ", "")
-
+print(new_par)
 for letter in new_par:
-    print(letter)
+    #print(letter)
     if letter in counts:
-        counts[letter] += 1
+        counts[letter] += 1        
     else:
         counts[letter] = 1
 
--- exercises/16-Techno_beat/app.py
+++ exercises/16-Techno_beat/app.py
@@ -1,6 +1,23 @@
 
 
 # Your code go above, nothing to change after this line:
+
+def lyrics_generator(listGen):
+    especial_lyrics = 0
+    lyrics = ""
+    for x in listGen:
+        if x == 1:
+            lyrics += "Drop the base "
+            especial_lyrics += 1
+            if especial_lyrics == 3:
+                lyrics += "!!!Break the base!!! "
+                especial_lyrics = 0
+        else:
+            lyrics += "Boom "
+
+    return lyrics
+            
+
 print(lyrics_generator([0,0,1,1,0,0,0]))
 print(lyrics_generator([0,0,1,1,1,0,0,0]))
 print(lyrics_generator([0,0,0]))
